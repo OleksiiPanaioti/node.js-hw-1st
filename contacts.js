fa4094d73511db2e335deffc32b0f9f2d0899807
OleksiiPanaioti <cavedzgn@gmail.com>
2023-06-21 14:32:27 +0300

    Finished task-1
    finished task-1

--- contacts.js
+++ contacts.js
@@ -1,1 +1,48 @@
-const fs = require("node:fs");
+const fs = require("fs/promises");
+const path = require("path");
+const { nanoid } = require("nanoid");
+
+const contactsPath = path.join(__dirname, "db", "contacts.json");
+
+const listContacts = async () => {
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
+};
+
+const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  const result = contacts.find((contact) => contact.id === contactId);
+  return result || null;
+};
+
+const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+
+  const removedContact = contacts.find((contact) => contact.id === contactId);
+  if (removedContact) {
+    const updatedContacts = contacts.filter(
+      (contact) => contact.id !== contactId
+    );
+    await fs.writeFile(contactsPath, JSON.stringify(updatedContacts, null, 2));
+  }
+  return removedContact || null;
+};
+
+const addContact = async (data) => {
+  const contacts = await listContacts();
+
+  const newContact = {
+    id: nanoid(),
+    ...data,
+  };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+};
+
+module.exports = {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+};
